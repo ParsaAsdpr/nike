b44d8ca3bad045089073c4ec00bdc05fa712520b
Parsa <parsaasadpour.1999@gmail.com>
2022-10-25 16:05:03 +0330

    slider buttons

--- utils/carouselButtons.js
+++ utils/carouselButtons.js
@@ -1,15 +1,16 @@
-import { BsArrowLeft } from "react-icons/bs";
+import { MdOutlineArrowForwardIos } from "react-icons/md";
 
 const CustomRightArrow = ({ onClick }) => {
 
     // onMove means if dragging or swiping in progress.
+    // className={`p-4 text-xl border text-stone-800 border-stone-800 rotate-180 absolute z-50
+    //      right-0 bottom-0 hover:text-white hover:bg-stone-800 transition hidden md:block`}
     return (
       <button
-        className={`p-4 text-xl border text-stone-800 border-stone-800 rotate-180 absolute z-50
-         right-0 bottom-0 hover:text-white hover:bg-stone-800 transition hidden md:block`}
+      className="text-stone-800 rounded-full p-2 absolute text-base bg-white hover:opacity-1 opacity-50 right-0 bottom-0 z-40"
         onClick={() => onClick()}
       >
-        <BsArrowLeft />
+        <MdOutlineArrowForwardIos />
       </button>
     );
   };
@@ -18,11 +19,10 @@ const CustomRightArrow = ({ onClick }) => {
     // onMove means if dragging or swiping in progress.
     return (
       <button
-        className="p-4 text-xl border text-stone-800 border-stone-800 absolute z-50
-         left-0 bottom-0 hover:text-white hover:bg-stone-800 transition hidden md:block"
+        className="text-stone-800 rounded-full p-2 absolute text-base bg-white opacity-50 rotate-180 hover:opacity-1 transition left-0 bottom-0 z-40"
         onClick={() => onClick()}
       >
-        <BsArrowLeft />
+        <MdOutlineArrowForwardIos />
       </button>
     );
   };
