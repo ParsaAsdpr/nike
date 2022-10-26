4cb1c522253607fdf6b475622257ec1f8fbe4233
Parsa <parsaasadpour.1999@gmail.com>
2022-10-26 18:46:01 +0330

    added search page

--- components/Navbar/SearchBox.jsx
+++ components/Navbar/SearchBox.jsx
@@ -1,12 +1,26 @@
 import React from 'react';
 import {BiSearchAlt2} from "react-icons/bi"
+import { useRouter } from 'next/router';
 
 const SearchBox = () => {
+  const [query, setQuery] = React.useState('');
+  const queryChangeHandler = (e) => {
+    setQuery(e.target.value);
+    console.log(query)
+  };
+
+    const router = useRouter();
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        router.push(`/search?query=${query}`);
+    }
     return (
-        <div className="rounded-full bg-white bg-opacity-20 text-gray-300 flex flex-row px-5 items-center">
-        <input type="text" className="w-full h-full rounded-full py-2 bg-transparent outline-none border-none text-right" placeholder="جستجو..."/>
-        <BiSearchAlt2 className="text-2xl cursor-pointer hover:text-white transition"></BiSearchAlt2>
-    </div>
+        <form onSubmit={handleSubmit} className="rounded-full bg-white bg-opacity-20 text-gray-300 flex flex-row px-5 items-center">
+        <input type="text" name='query' onChange={queryChangeHandler} className="w-full h-full focus:bg-transparent autofill:bg-transparent py-2 bg-transparent outline-none border-none text-right" placeholder="جستجو..."/><button type='submit'>
+            <BiSearchAlt2 className="text-2xl cursor-pointer hover:text-white transition"></BiSearchAlt2>
+        </button>
+    </form>
     );
 };
 
---
+++ components/Products/FilterDropDown.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const FilterDropDown = (props) => {
+  return (
+    <div className="flex flex-row gap-3 items-center">
+        <p className="text-stone-100 text-xl">{props.text}</p>
+      <select
+        className="py-3 px-4 text-lg rounded-md bg-stone-900 text-stone-300 border-l-8 border-l-transparent"
+        value={props.defaultValue}
+        onChange={props.changeHandler}
+      >
+        <option value="all">همه</option>
+        {props.options &&
+          props.options.map((option, index) => (
+            <option key={index} value={option}>
+              {option}
+            </option>
+          ))}
+      </select>
+    </div>
+  );
+};
+
+export default FilterDropDown;
